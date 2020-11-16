99d383d83281846fef8ceef0a81981cf4238a776
elisabethzinck <s164204@student.dtu.dk>
2020-11-16 15:46:37 +0100

    Update create_data_structure.py

--- Src/Data/create_data_structure.py
+++ Src/Data/create_data_structure.py
@@ -5,10 +5,69 @@ Created on Mon Nov  9 20:28:40 2020
 @author: elisa
 """
 
-import pandas as pd
 from pathlib import Path
+from datasets import load_from_disk
 
 #%%
 path = Path(__file__).parent.parent.parent
 #%%
-tmp = pd.read_pickle(path/'Data/Raw/raw_trivia.pickle')
+data = load_from_disk(path/'Data/Raw/')
+
+#%%
+def preprocess_data(data, text_length, remove_search_results):
+    """ 
+    Takes the raw data and creates paragraph dataset
+
+    
+    """
+    if remove_search_results:
+        print('Hovsa, det skal du selv goere.')
+    
+    out_data = data.map(lambda example: {'wiki_text': example['entity_pages']['wiki_context'], 
+                                             'answer': example['answer']['normalized_value']}, 
+                            remove_columns=['question_source', 'answer', 'entity_pages'])
+    out_data = out_data.filter(lambda example: len(example['wiki_text']) > 0)
+    out_data = out_data.map(lambda example: {'paragraph': get_paragraph(example)})
+    out_data = out_data.map(remove_columns = ['wiki_text'])
+    
+    return(out_data)
+
+def get_paragraph(example):
+    text_length = 128
+    n_docs = len(example['wiki_text'])
+    for i in range(n_docs):
+        idx = example['wiki_text'][i].lower().find(example['answer'])
+        if idx != -1:
+            idx_lwr = idx-text_length
+            idx_upr = idx+text_length
+            if idx_lwr < 0:
+                idx_lwr = 0
+                idx_upr = 2*text_length
+            elif idx_upr > len(example['wiki_text'][i]):
+                idx_upr = len(example['wiki_text'][i])
+                idx_lwr = idx_upr - 2*text_length
+            paragraph = example['wiki_text'][i].lower()[idx_lwr:idx_upr]
+            return(paragraph)
+    
+
+#%%
+tmp = preprocess_data(data, 128, False)
+
+#%%
+example = tmp[1]
+text_length = 64
+paragraph = ''
+n_docs = len(example['wiki_text'])
+for i in range(n_docs):
+    idx = example['wiki_text'][i].lower().find(example['answer'])
+    if idx != -1:
+        paragraph = example['wiki_text'][i].lower()[(idx-text_length):(idx+text_length)]
+        break
+
+#%%
+
+
+
+
+
+
